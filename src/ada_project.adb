bb6571587a3a5208989a9f5f681119ce338851b5
Yankie <yankielpachecochanfrau@yahoo>
2023-04-27 22:53:04 +0200

    Initial commit

---
+++ src/ada_project.adb
@@ -0,0 +1,13 @@
+--  ada-project.adb
+
+-- import system libraries
+with Ada.Text_IO;
+
+procedure ada_project is
+-- espacio para declarar las variables a utilizar
+
+begin
+    Ada.Text_IO.Put_Line("Hello world!");
+    Ada.text_IO.Put("It's a wonderful day!");
+    Ada.text_IO.New_Line; -- crea nueva linea
+end ada_project;
